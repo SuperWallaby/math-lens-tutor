34a9045b40f5bbec563c2a1f8d560a1c750978ec
superwallaby <colton950901@gmail.com>
2026-08-15 18:55:35 +0900

    Show pin HTML immediately so the screen is not a blank Flutter canvas.

--- flutter_app/lib/widgets/pin_html_overlay_web.dart
+++ flutter_app/lib/widgets/pin_html_overlay_web.dart
@@ -72,6 +72,9 @@ class _PinHtmlOverlayState extends State<PinHtmlOverlay> {
   }
 
   void _mount() {
+    if (html.document.getElementById('wooyeol-pin') != null) {
+      return;
+    }
     final frame = html.IFrameElement()
       ..src = _srcFor(widget.screen)
       ..style.position = 'fixed'
